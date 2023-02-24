11e12b8f9a423130e2895c913cbc6cbc103a7d50
carli0 <olliecarlile@hotmail.com>
2023-02-24 10:24:01 +0000

    Added patient registration

---
+++ analysis/report.do
@@ -0,0 +1,5 @@
+!gunzip output/input.csv.gz
+import delimited using output/input.csv
+
+// TODO plot equivalent
+// TODO save plot
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -10,4 +10,37 @@ study = StudyDefinition(
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
     ),
+
+    age=patients.age_as_of(
+        "2019-09-01",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
+    ),
+)
+
+registered = patients.registered_as_of(
+    "2020-03-01",
+    return_expectations={"incidence": 0.98}
+)
+
+region = patients.registered_practice_as_of(
+    "2020-02-01",
+    returning="nuts1_region_name",
+    return_expectations={
+        "rate": "universal",
+        "category": {
+            "ratios": {
+                "North East": 0.1,
+                "North West": 0.1,
+                "Yorkshire and the Humber": 0.1,
+                "East Midlands": 0.1,
+                "West Midlands": 0.1,
+                "East of England": 0.1,
+                "London": 0.2,
+                "South East": 0.2,
+            },
+        },
+    },
 )
